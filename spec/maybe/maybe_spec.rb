d43b8b87e79d4416375becf5be7c205c125fbb03
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-05 12:17:42 +0100

    Improves readability of test names

--- spec/maybe/maybe_spec.rb
+++ spec/maybe/maybe_spec.rb
@@ -3,7 +3,7 @@ require 'maybe'
 RSpec.describe Maybe do
   describe 'do method for' do
     describe Maybe::This do
-      it 'will execute side effects with its value' do
+      it 'executes side effects with its value' do
         this_value = Maybe::This.new("value")
 
         side_effect = "nothing happened"
@@ -12,7 +12,7 @@ RSpec.describe Maybe do
         expect(side_effect).to eq("done with: value")
       end
 
-      it 'will return itself' do
+      it 'returns itself' do
         this_value = Maybe::This.new("value")
         returned_value = this_value.do {}
         expect(returned_value).to be(this_value)
@@ -20,7 +20,7 @@ RSpec.describe Maybe do
     end
 
     describe Maybe::Nothing do
-      it 'will not execute side effects' do
+      it 'does not execute side effects' do
         nothing = Maybe::Nothing
 
         side_effect = "nothing happened"
@@ -29,7 +29,7 @@ RSpec.describe Maybe do
         expect(side_effect).to eq("nothing happened")
       end
 
-      it 'will return itself' do
+      it 'returns itself' do
         nothing = Maybe::Nothing
         returned_value = nothing.do {}
         expect(returned_value).to be(nothing)
@@ -39,7 +39,7 @@ RSpec.describe Maybe do
 
   describe 'or method for' do
     describe Maybe::This do
-      it 'will return its value' do
+      it 'returns its value' do
         this_value = Maybe::This.new("value")
         returned_value = this_value.or {"alternative value"}
         expect(returned_value).to eq("value")
@@ -47,7 +47,7 @@ RSpec.describe Maybe do
     end
 
     describe Maybe::Nothing do
-      it 'will return the alternative value' do
+      it 'returns the alternative value' do
         nothing = Maybe::Nothing
         returned_value = nothing.or {"alternative value"}
         expect(returned_value).to eq("alternative value")
